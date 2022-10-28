b12e32cf5479eab87d63686f45a14ffaebfb06fd
Matteo <matteofusaro1@gmail.com>
2022-10-28 15:31:43 +0200

    FunnyTest commit

---
+++ src/test/java/it/unical/mat/ProvaIngSw_203788/FunnyAlgorithmsTest.java
@@ -0,0 +1,20 @@
+package it.unical.mat.ProvaIngSw_203788;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+public class FunnyAlgorithmsTest {
+	
+	private static FunnyAlgorithms fa;
+	
+	
+	@Before
+	public void before() {
+		fa = new FunnyAlgorithms();
+	}
+	
+	@BeforeClass
+	public void beforeClass() {
+		//take time with joda time
+	}
+}
