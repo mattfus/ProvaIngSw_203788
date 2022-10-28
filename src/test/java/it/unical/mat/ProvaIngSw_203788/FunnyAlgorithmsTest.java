5e30b0151054f81ca757c256b1407e4a531bce26
Matteo <matteofusaro1@gmail.com>
2022-10-28 15:46:23 +0200

    Test case update

--- src/test/java/it/unical/mat/ProvaIngSw_203788/FunnyAlgorithmsTest.java
+++ src/test/java/it/unical/mat/ProvaIngSw_203788/FunnyAlgorithmsTest.java
@@ -1,7 +1,10 @@
 package it.unical.mat.ProvaIngSw_203788;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class FunnyAlgorithmsTest {
 	
@@ -17,4 +20,19 @@ public class FunnyAlgorithmsTest {
 	public void beforeClass() {
 		//take time with joda time
 	}
+	
+	@Test
+	public void stringToIntConverterWorksFine() {
+		assertEquals(1, fa.stringToIntConverter("1"));
+		assertEquals(10, fa.stringToIntConverter("10"));
+		assertEquals(-55, fa.stringToIntConverter("-55"));
+		assertEquals(-32768, fa.stringToIntConverter("-32768"));
+		assertEquals(32767, fa.stringToIntConverter("32767"));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void stringToIntConverterThrowsException() {
+		fa.stringToIntConverter("banana33");
+	}
+	
 }
