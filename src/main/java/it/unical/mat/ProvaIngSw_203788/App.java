0d285d73afaba1563771e2c88ce7eb4bedab5840
Matteo <matteofusaro1@gmail.com>
2022-10-28 15:11:09 +0200

    first commit -> branch master

---
+++ src/main/java/it/unical/mat/ProvaIngSw_203788/App.java
@@ -0,0 +1,13 @@
+package it.unical.mat.ProvaIngSw_203788;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
